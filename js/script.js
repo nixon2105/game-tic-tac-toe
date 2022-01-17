d007f4c6b468a9e1fe8e4c4a1c50d7ef0164d61f
nixon2105 <radivilov1991@mail.ru>
2022-01-17 08:36:09 +0300

    feat: add function check and layout for modal

--- js/script.js
+++ js/script.js
@@ -1,9 +1,46 @@
 const area = document.getElementById('area');
 let move = 0;
+let result = '';
 
 area.addEventListener('click', (e) => {
   if (e.target.className === 'box') {
     move % 2 === 0 ? (e.target.innerHTML = 'x') : (e.target.innerHTML = '0');
     move += 1;
+    check();
   }
 });
+
+const check = () => {
+  const boxes = document.getElementsByClassName('box');
+  const arr = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (
+      boxes[arr[i][0]].innerHTML == 'x' &&
+      boxes[arr[i][1]].innerHTML == 'x' &&
+      boxes[arr[i][2]].innerHTML == 'x'
+    ) {
+      result = 'крестики';
+      prepareResult(result);
+    } else if (
+      boxes[arr[i][0]].innerHTML == '0' &&
+      boxes[arr[i][1]].innerHTML == '0' &&
+      boxes[arr[i][2]].innerHTML == '0'
+    ) {
+      result = 'нолики';
+      prepareResult(result);
+    }
+  }
+};
+
+const prepareResult = (winner) => {
+  console.log(winner);
+};
